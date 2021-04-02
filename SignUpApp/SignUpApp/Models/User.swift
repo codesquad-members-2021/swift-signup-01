d6e4ea00357d021ca637df4cb88ea870aa11a1bb
zombietux <>
2021-04-02 16:18:14 +0900

    사용하지 않는 모델 병합

--- SignUpApp/SignUpApp/Models/User.swift
+++ SignUpApp/SignUpApp/Models/User.swift
@@ -11,22 +11,34 @@ class User: UserManageable {
     private let id: ID
     private let password: Password
     private let passwordConfirm: PasswordConfirm
-    private let information: Information
+    private let name: Name
+    private let email: Email
+    private let birth: Birth
+    private let phoneNumber: PhoneNumber
+    private let interests: Interests
     
-    init(id: ID, password: Password, information: Information, passwordConfirm: PasswordConfirm) {
+    init(id: ID, password: Password, passwordConfirm: PasswordConfirm, name: Name, email: Email, birth: Birth, phoneNumber: PhoneNumber, interests: Interests) {
         self.id = id
         self.password = password
         self.passwordConfirm = passwordConfirm
-        self.information = information
+        self.name = name
+        self.email = email
+        self.birth = birth
+        self.phoneNumber = phoneNumber
+        self.interests = interests
     }
     
     convenience init() {
         let id = ID()
         let password = Password()
         let passwordConfirm = PasswordConfirm()
-        let information = Information(name: Name(), email: Email(), birth: Birth(), phoneNumber: PhoneNumber(), interest: [])
+        let interests = Interests()
+        let name = Name()
+        let email = Email()
+        let birth = Birth()
+        let phoneNumber = PhoneNumber()
         
-        self.init(id: id, password: password, information: information, passwordConfirm: passwordConfirm)
+        self.init(id: id, password: password, passwordConfirm: passwordConfirm, name: name, email: email, birth: birth, phoneNumber: phoneNumber, interests: interests)
     }
     
     func isValidId(input id: String) -> (Bool, String) {
